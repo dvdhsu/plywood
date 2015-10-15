bf0a80ee39078c18880807aead0494500ebf45bb
David Hsu <dvdhsu@gmail.com>
2015-10-15 03:29:49 +0100

    WIP for adding location on login.
    The remaining problem is what to do when the location cannot be parsed.
    Currently it throws an exception, which results in a 500. We probably want to
    change this to a 400 Bad Request. Question is where to catch the exception and
    render the error.

--- web/controllers/authentication_controller.ex
+++ web/controllers/authentication_controller.ex
@@ -4,7 +4,8 @@ defmodule Melamine.AuthenticationController do
   alias Melamine.Authentication
   alias Melamine.User
 
-  def login_or_create(conn, %{ "facebook_token" => facebook_token }) do
+  def login_or_create(conn, %{ "facebook_token" => facebook_token } = params) do
+    inspect params
     case Facebook.me("email,name", facebook_token) do
       {_, %{ "error" => error }} ->
         conn
@@ -17,12 +18,12 @@ defmodule Melamine.AuthenticationController do
               |> Dict.put("auth_tokens", [User.new_token])
               |> Dict.put("facebook_id", facebook_user["id"])
               |> Dict.put("facebook_token", facebook_token)
+              |> Dict.put("last_location", params_location(params))
               |> Dict.delete("id")
             create_user(conn, user_params)
           user ->
             auth_tokens = [User.new_token | user.auth_tokens]
-            user = User.changeset(user, %{ auth_tokens: auth_tokens, facebook_token: facebook_token })
-
+            user = User.changeset(user, %{ auth_tokens: auth_tokens, facebook_token: facebook_token, last_location: params_location(params) })
             case Repo.update user do
               {:ok, user} ->
                 conn
@@ -53,6 +54,14 @@ defmodule Melamine.AuthenticationController do
         conn
           |> put_status(:unprocessable_entity)
           |> render(Melamine.ChangesetView, "error.json", changeset: changeset)
+    end
+  end
+
+  defp params_location(params) do
+    if params["location"] && String.length(params["location"]) > 0 do
+      Geo.WKT.decode(params["location"])
+    else
+      nil
     end
   end
 end
